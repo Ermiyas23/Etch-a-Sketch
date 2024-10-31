76eb14f9aee23461f8df811d39021a3c4d762df6
Ermiyas23 <ermi23@on.com>
2024-10-31 07:29:39 -0700

    Create container is done

---
+++ script.js
@@ -0,0 +1,7 @@
+const bug = document.querySelector('body');
+const div = document.createElement('div');
+
+bug.appendChild(div);
+
+const container = document.createElement('div');
+div.appendChild(container);
